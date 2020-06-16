e881c6b959e64775dd95967668e49e70b6a5fc38
Bachinina <olya.bachinina@yandex.ru>
2020-06-16 11:14:20 +0300

    add prop-types

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
+const movies = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`];
 
 const movieData = {
   title: `The Grand Budapest Hotel`,
@@ -14,6 +15,7 @@ ReactDOM.render(
       title={movieData.title}
       genre={movieData.genre}
       year={movieData.year}
+      movies={movies}
     />,
     document.querySelector(`#root`)
 );
